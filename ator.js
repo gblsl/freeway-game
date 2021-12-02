fe194825500930ccbeb372e4565d12b66879c96e
gblsl <gblsl@protonmail.ch>
2021-12-02 13:56:46 -0300

    adicionando placar

--- ator.js
+++ ator.js
@@ -1,7 +1,8 @@
 
-let xAtor = 100;
+let xAtor = 90;
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor() {
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -23,11 +24,25 @@ function verificaColisao() {
   for(let i = 0; i < imagemCarros.length; i++) {
     colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
     if (colisao) {
-      colidiu();
+      voltaAtorParaPosicaoInicial();
     }
   }
 }
 
-function colidiu() {
+function voltaAtorParaPosicaoInicial() {
   yAtor = 366;
+}
+
+function incluiPontos() {
+  textAlign(CENTER);
+  textSize(23);
+  fill(255, 240, 60);
+  text("Pontos: " + meusPontos, width / 5, 26);
+}
+
+function marcaPonto() {
+  if (yAtor < 2) {
+    meusPontos++;
+    voltaAtorParaPosicaoInicial();
+  }
 }
